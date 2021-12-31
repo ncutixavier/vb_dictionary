c263a84a334af1246b39a40190b645b67eb822ef
ncutixavier <ncuti60@gmail.com>
2021-12-31 14:30:57 +0200

    initial commit

---
+++ dictionary/dictionary/Dictionary.Designer.vb
@@ -0,0 +1,188 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle2 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle3 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.txt_word = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txt_meaning = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btn_add = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.txt_search = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txt_word
+        '
+        Me.txt_word.Font = New System.Drawing.Font("Microsoft Tai Le", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_word.Location = New System.Drawing.Point(8, 26)
+        Me.txt_word.Name = "txt_word"
+        Me.txt_word.Size = New System.Drawing.Size(200, 24)
+        Me.txt_word.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Tai Le", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(4, 6)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(52, 19)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Word*"
+        '
+        'txt_meaning
+        '
+        Me.txt_meaning.Font = New System.Drawing.Font("Microsoft Tai Le", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_meaning.Location = New System.Drawing.Point(255, 26)
+        Me.txt_meaning.Name = "txt_meaning"
+        Me.txt_meaning.Size = New System.Drawing.Size(517, 24)
+        Me.txt_meaning.TabIndex = 0
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Tai Le", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(251, 6)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(73, 19)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Meaning*"
+        '
+        'btn_add
+        '
+        Me.btn_add.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.btn_add.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btn_add.Location = New System.Drawing.Point(803, 22)
+        Me.btn_add.Name = "btn_add"
+        Me.btn_add.Size = New System.Drawing.Size(76, 30)
+        Me.btn_add.TabIndex = 2
+        Me.btn_add.Text = "ADD"
+        Me.btn_add.UseVisualStyleBackColor = False
+        '
+        'Panel1
+        '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel1.Controls.Add(Me.txt_meaning)
+        Me.Panel1.Controls.Add(Me.btn_add)
+        Me.Panel1.Controls.Add(Me.txt_word)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Location = New System.Drawing.Point(12, 41)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(886, 67)
+        Me.Panel1.TabIndex = 3
+        '
+        'txt_search
+        '
+        Me.txt_search.AcceptsTab = True
+        Me.txt_search.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.txt_search.Font = New System.Drawing.Font("Microsoft Tai Le", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_search.ForeColor = System.Drawing.SystemColors.WindowFrame
+        Me.txt_search.Location = New System.Drawing.Point(628, 114)
+        Me.txt_search.Name = "txt_search"
+        Me.txt_search.Size = New System.Drawing.Size(270, 24)
+        Me.txt_search.TabIndex = 3
+        Me.txt_search.Text = "Search"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace
+        Me.DataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        DataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control
+        DataGridViewCellStyle1.Font = New System.Drawing.Font("Microsoft Tai Le", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText
+        DataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.DataGridView1.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle1
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.GridColor = System.Drawing.Color.Silver
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 144)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        DataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control
+        DataGridViewCellStyle2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText
+        DataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.ControlLightLight
+        DataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.DataGridView1.RowHeadersDefaultCellStyle = DataGridViewCellStyle2
+        Me.DataGridView1.RowHeadersVisible = False
+        DataGridViewCellStyle3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.White
+        DataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.Black
+        Me.DataGridView1.RowsDefaultCellStyle = DataGridViewCellStyle3
+        Me.DataGridView1.ShowEditingIcon = False
+        Me.DataGridView1.Size = New System.Drawing.Size(886, 347)
+        Me.DataGridView1.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Stencil", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(358, 6)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(184, 32)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "DICTIONARY"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(910, 503)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.txt_search)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.Text = "Dictionary"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txt_word As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txt_meaning As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents btn_add As System.Windows.Forms.Button
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents txt_search As System.Windows.Forms.TextBox
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+
+End Class
---
+++ dictionary/dictionary/Dictionary.vb
@@ -0,0 +1,122 @@
+﻿Imports System.Data.OleDb
+Public Class Form1
+
+    Dim con As New OleDb.OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=c:\users\ncuti\Documents\Dictionary.accdb")
+
+    Private Sub txt_search_GotFocus(sender As Object, e As EventArgs) Handles txt_search.GotFocus
+        If txt_search.Text = "search" Then
+            txt_search.Text = ""
+        End If
+    End Sub
+
+    Private Sub txt_search_LostFocus(sender As Object, e As EventArgs) Handles txt_search.LostFocus
+        If txt_search.Text = "" Then
+            txt_search.Text = "search"
+        End If
+    End Sub
+
+    Sub DisplayData(ByVal table_name As String, ByVal data_grid_name As DataGridView, ByVal sort_item As String)
+        Try
+            Dim sql As String
+            Dim cmd As New OleDb.OleDbCommand
+            Dim dt As New DataTable
+            Dim da As New OleDb.OleDbDataAdapter
+            sql = "select * from " + table_name + " order by " + sort_item
+            cmd.Connection = con
+            cmd.CommandText = sql
+            da.SelectCommand = cmd
+            da.Fill(dt)
+            data_grid_name.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
+            data_grid_name.DataSource = dt
+            data_grid_name.Columns(0).Visible = False
+            data_grid_name.Columns(1).Width = 300
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        Finally
+            con.Close()
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call DisplayData("dictionary", DataGridView1, "Word")
+    End Sub
+
+    Function checkDuplicate(ByVal word As String)
+        Dim sqlQRY As String = "SELECT COUNT(*) AS numRows FROM dictionary WHERE Word = '" & word & "'"
+        Dim queryResult As Integer
+
+        con.Open()
+
+        Dim com As New OleDb.OleDbCommand(sqlQRY, con)
+        queryResult = com.ExecuteScalar()
+
+        con.Close()
+
+        If queryResult > 0 Then
+            MsgBox("Word is already exist")
+            Return False
+        Else
+            Return True
+        End If
+    End Function
+
+    Private Sub btn_add_Click(sender As Object, e As EventArgs) Handles btn_add.Click
+        If txt_meaning.Text = "" Or txt_word.Text = "" Then
+            MsgBox("Please fill all fields!")
+
+        ElseIf checkDuplicate(txt_word.Text) Then
+            Try
+                Dim sql As String
+                Dim cmd As New OleDb.OleDbCommand
+                Dim i As Integer
+                con.Open()
+                sql = "INSERT INTO dictionary (Word, Meaning) VALUES ('" & txt_word.Text & "', '" & txt_meaning.Text & "');"
+                cmd.Connection = con
+                cmd.CommandText = sql
+                i = cmd.ExecuteNonQuery
+                If i > 0 Then
+                    MsgBox("Successfull Added!")
+                    ''clear fields
+                    txt_meaning.Text = ""
+                    txt_word.Text = ""
+                    Call DisplayData("dictionary", DataGridView1, "Word")
+                Else
+                    MsgBox("Unable to insert into dictionary")
+                End If
+
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            Finally
+                con.Close()
+
+            End Try
+        End If
+    End Sub
+
+    Sub searchProduct(ByVal table_name As String, ByVal data_grid_name As DataGridView, ByVal word As String, ByVal word_field As String)
+        Try
+            Dim sql As String
+            Dim cmd As New OleDb.OleDbCommand
+            Dim dt As New DataTable
+            Dim da As New OleDb.OleDbDataAdapter
+            sql = "select * from " + table_name + " where " + word_field + " like '%" & word & "%' order by " + word_field
+            cmd.Connection = con
+            cmd.CommandText = sql
+            da.SelectCommand = cmd
+            dt.Clear()
+
+            da.Fill(dt)
+            data_grid_name.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
+            data_grid_name.DataSource = dt
+            'data_grid_name.Columns(0).Visible = False
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        Finally
+            con.Close()
+        End Try
+    End Sub
+
+    Private Sub txt_search_TextChanged(sender As Object, e As EventArgs) Handles txt_search.TextChanged
+        Call searchProduct("dictionary", DataGridView1, txt_search.Text, "Word")
+    End Sub
+End Class
